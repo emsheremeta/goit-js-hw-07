4fe1c794f2b2d88b4ac0a8addf81d35f3140e0a2
Marina <sheremetamaryna@gmail.com>
2022-04-30 22:52:31 +0200

    task 1

---
+++ js/01-gallery.js
@@ -0,0 +1,38 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+// зарендерить все 
+const addGalleryItems = document.querySelector('.gallery');
+const galleryMarup = createGalleryItems (galleryItems);// хранит результат функции
+
+addGalleryItems.insertAdjacentHTML("beforebegin", galleryMarup);
+addGalleryItems.addEventListener("click", handleGalleryClick)
+
+function createGalleryItems (galleryItems) {
+    return galleryItems.map (({preview, original, description }) => {
+        return `
+        <div class="gallery__item">
+        <a class="gallery__link" href="large-image.jpg">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>
+      `;
+    })
+    .join('');
+  
+};
+
+function handleGalleryClick (event) {
+   const isGalleryItem = event.target.classList.contains('.gallery__item');
+
+    if (!isGalleryItem){
+        return;
+    }
+   return event.target.dataset.original; 
+}
